ef17be4d3e342744377e2128d158d3675b391bcd
vlad <g-star.raw@live.se>
2023-09-19 13:31:00 +0200

    basic Scanner Example

---
+++ src/com/sertan/scanner/Main.java
@@ -0,0 +1,27 @@
+package com.sertan.scanner;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        do {
+
+            System.out.println("Input Number");
+            int result = sc.nextInt();
+
+            System.out.println("You picked: " +  result);
+
+        } while (true);
+
+        
+
+            
+    }
+
+
+
+    }
