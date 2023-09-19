d9181c7b3dfbee22490f5f5f15532edd43b8bec5
vlad <g-star.raw@live.se>
2023-09-19 13:41:55 +0200

    student Scanner implementation

--- src/com/sertan/scanner/Main.java
+++ src/com/sertan/scanner/Main.java
@@ -6,14 +6,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        // git push =
         Scanner sc = new Scanner(System.in);
+        Student benny = new Student();
 
         do {
 
-            System.out.println("Input Number");
+            System.out.println("Input benny's age!");
             int result = sc.nextInt();
 
+            benny.age = result;
+
             System.out.println("You picked: " +  result);
+            System.out.println("Benny's age is: " + benny.age);
+
 
         } while (true);
 
---
+++ src/com/sertan/scanner/Student.java
@@ -0,0 +1,9 @@
+package com.sertan.scanner;
+
+public class Student {
+
+    public String name;
+    public int age;
+    public Boolean IsTired;
+
+}
